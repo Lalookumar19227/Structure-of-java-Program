0fb674d7d013646456090715a6002423f8d52181
Lalookumar19227 <imykl27@gmail.com>
2024-12-09 14:05:23 +0530

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+class FirstProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Java Program");
+		System.out.print("12+50+40*4=");
+		System.out.println(12+50+40*4);
+		System.out.println("Now Multiply");
+		System.out.print("12*5*8=");
+		System.out.println(12*5*8);
+		System.out.println("Program Completed");
+		System.out.println("Thank You!!");		
+	}
+}
---
+++ Program1.java
@@ -0,0 +1,9 @@
+class Program1
+{
+	public static void main(String[] piyush)
+	{
+		System.out.println("jAvA ProGram".toLowerCase());
+		System.out.println("jAvA ProGram".toUpperCase());
+				
+	}
+}
